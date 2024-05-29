53c2d9c7b3078c69fefe299e4c10865f4e7e87be
uyeon <65580604+uyeonyx@users.noreply.github.com>
2024-05-29 14:30:27 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,11 @@ zshrc() {
     git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
     ~/.fzf/install​
+    mkdir -p $HOME/code
+    mkdir -p $HOME/code/snippets
     mv -f $HOME/dots/zshrc $HOME/.zshrc
     mv -f $HOME/dots/p10k.zsh $HOME/.p10k.zsh
+    mv -f $HOME/snippets $HOME/code/snippets
 }
 
 
@@ -19,13 +22,9 @@ sudo dpkg-reconfigure --frontend noninteractive tzdata
 
 zshrc
 
-echo 'exec zsh' >> ~/.bashrc
-source ~/.bashrc
-
 # make directly highlighting readable - needs to be after zshrc line
 echo "" >> ~/.zshrc
 echo "# remove ls and directory completion highlight color" >> ~/.zshrc
 echo "_ls_colors=':ow=01;33'" >> ~/.zshrc
 echo 'zstyle ":completion:*:default" list-colors "${(s.:.)_ls_colors}"' >> ~/.zshrc
 echo 'LS_COLORS+=$_ls_colors' >> ~/.zshrc
-source ~/.zshrc
