b99eb54e41b3d861a73d52afc1ac8b1637ae40e9
uyeon <65580604+uyeonyx@users.noreply.github.com>
2024-05-29 14:47:58 +0900

    Update p10k.zsh

--- dots/p10k.zsh
+++ dots/p10k.zsh
@@ -984,7 +984,7 @@
   # Context format when running with privileges: user@hostname.
   typeset -g POWERLEVEL9K_CONTEXT_ROOT_TEMPLATE='%n@%m'
   # Context format when in SSH without privileges: user@hostname.
-  typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_TEMPLATE='%n@%16<..<m'
+  typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_TEMPLATE='%n@remote'
   # Default context format (no privileges, no SSH): user@hostname.
   typeset -g POWERLEVEL9K_CONTEXT_TEMPLATE='%n@%m'
 
