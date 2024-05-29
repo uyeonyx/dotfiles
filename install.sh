3a96fb7e268b0e76a0ecdb51a38e972a47e28e26
uyeon <65580604+uyeonyx@users.noreply.github.com>
2024-05-29 11:14:45 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-mkdir -p /home/codespace/test
 zshrc() {
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
