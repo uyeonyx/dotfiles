190ac5173feef7eb58c77ff63ec469689d4ddc67
uyeon <65580604+uyeonyx@users.noreply.github.com>
2024-06-08 23:18:16 +0900

    Update after-init.sh

--- workspace/after-init.sh
+++ workspace/after-init.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
-read -p "Please enter your OPENAI_API_KEY: " OPENAI_API_KEY
+echo -n "Please enter your OPENAI_API_KEY: "
+read OPENAI_API_KEY
 echo "export ZSH_ASK_API_KEY=\"$OPENAI_API_KEY\"" >> ~/.zshrc
 source ~/.zshrc
 echo "ZSH_ASK_API_KEY has been added to your environment variables."
