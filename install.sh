0ad7f6304630e780cc47f98b38bddddea5381ef2
uyeon <65580604+uyeonyx@users.noreply.github.com>
2024-06-08 20:16:48 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,10 +8,12 @@ zshrc() {
     git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
     yes | sudo sh -c ~/.fzf/install
+    git clone https://github.com/wting/autojump.git /tmp/autojump
+    CURRENT_DIR=$(pwd) && cd /tmp/autojump && ./install.py && cd $CURRENT_DIR && rm -rf /tmp/auto-jump
+    git clone https://github.com/Licheam/zsh-ask ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-ask
     mv -f $HOME/dots/zshrc $HOME/.zshrc
     mv -f $HOME/dots/p10k.zsh $HOME/.p10k.zsh
 }
-
 
 # change time zone
 sudo ln -fs /usr/share/zoneinfo/America/Chicago /etc/localtime
@@ -19,6 +21,7 @@ sudo dpkg-reconfigure --frontend noninteractive tzdata
 
 zshrc
 
+# force zsh
 echo 'exec zsh' >> ~/.bashrc
 source ~/.bashrc
 
@@ -29,3 +32,6 @@ echo "_ls_colors=':ow=01;33'" >> ~/.zshrc
 echo 'zstyle ":completion:*:default" list-colors "${(s.:.)_ls_colors}"' >> ~/.zshrc
 echo 'LS_COLORS+=$_ls_colors' >> ~/.zshrc
 
+rm -rf ~/dots
+rm -rf ~/README.md
+rm -rf ~/install.sh
